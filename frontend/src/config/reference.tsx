e9bfd1063359f48cd9e477ca8da4190302f2b39d
defenderwidi <defenderwidi17@gmail.com>
2026-05-12 17:12:50 +0800

    feat: update reference heights for B2000 and B1250 in REF_PRESET_M

--- frontend/src/config/reference.tsx
+++ frontend/src/config/reference.tsx
@@ -2,14 +2,14 @@
 
 // Reference height tiap unit (meter)
 export const REF_PRESET_M = {
-  B2000: 7.03,
+  B2000: 5.97,
   B6015: 5.25,
   B6020: 6.37,
   S3600: 7.83,
   B3600: 7.83,
   B2500: 7.04,
   S2500: 7.04,
-  B1250: 4.12,
+  B1250: 5.18,
   S4000: 8.30,
   B4000: 8.30,
 } as const
